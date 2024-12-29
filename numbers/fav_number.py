506eb31db3a921040b9a2f993c3cba779423098f
rp.parthi <rp.paarthi@gmail.com>
2024-12-29 18:42:06 +0530

    learn numbers

---
+++ numbers/fav_number.py
@@ -0,0 +1,3 @@
+#program to represent my favorite number
+fav_number = 3
+print(f"My favorite number is {fav_number}. ")
---
+++ numbers/number_eight.py
@@ -0,0 +1,6 @@
+#Pp 20-08
+#exercise to output 8
+print(5+3)
+print(10-2)
+print(4*2)
+print(16/2)
